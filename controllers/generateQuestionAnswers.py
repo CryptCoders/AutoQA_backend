4760bcd78934733a0ea1127275bd2df5c1f8752c
JoKeR-VIKING <prathamvasani1@gmail.com>
2024-05-06 19:04:45 +0530

    requirements updated

--- controllers/generateQuestionAnswers.py
+++ controllers/generateQuestionAnswers.py
@@ -1,9 +1,8 @@
-import threading, os
+import os, queue, threading
 from flask import jsonify, request
 from flask_restful import Resource
 from utils.generateQuestions import generateQuestions
 from utils.generateAnswers import generateAnswer
-from collections import deque
 
 class GenerateQuestionAnswers(Resource):
     @staticmethod
@@ -12,17 +11,43 @@ class GenerateQuestionAnswers(Resource):
         file.save('temp.pdf')
 
         file_content, questions = generateQuestions('temp.pdf')
-        api_keys = deque(os.getenv("GOOGLE_API_KEYS").split(','))
+        api_keys = os.getenv("GOOGLE_API_KEYS").split(',')
 
         response = []
 
-        for question in questions:
-            api_keys.append(api_keys[0])
-            answer = generateAnswer(file_content, question, api_keys.popleft())
+        api_queue = queue.Queue()
+        for api_key in api_keys:
+            api_queue.put(api_key)
 
-            response.append({
-                "question": question,
-                "answer": answer
-            })
+        keys_lock = threading.Lock()
+
+        def multithreading(question):
+            api_key = None
+
+            with keys_lock:
+                while api_queue.empty():
+                    pass
+
+                api_key = api_queue.get(block = True)
+                answer = generateAnswer(file_content, question, api_key)
+
+                with keys_lock:
+                    api_queue.put(api_key)
+
+                return answer
+
+        threads = []
+
+        for question in questions:
+            thread = threading.Thread(target = lambda q=question: response.append({
+                "question": q,
+                "answer": multithreading(q)
+            }))
+
+            threads.append(thread)
+            thread.start()
+
+        for thread in threads:
+            thread.join()
 
         return jsonify({ 'data': response })
